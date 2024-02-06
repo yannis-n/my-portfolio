b4ebf62f2470cc3c3d31166525989323a4410f16
yannis_n <gnikolaidis@jetpack.vc>
2024-02-06 23:45:11 +0200

    lower mask time

--- resources/js/components/Introduction.js
+++ resources/js/components/Introduction.js
@@ -3,16 +3,12 @@ function Introduction(props) {
     
     return (
         <section aria-label="Introduction" id="introduction" className={" relative flex justify-center flex-col items-start"+(props.siteEntered ? ' moved-in' : ' opacity-0')}>
-            <h1 className="w-full  text-4xl md:text-6xl font-bold  details-color">
-                Hi,
+            <h1  className="w-full text-4xl md:text-6xl  font-bold whitespace-nowrap details-color">
+                I am Yannis.
             </h1>
 
-            <h2  className="w-full text-4xl md:text-6xl  font-bold whitespace-nowrap details-color">
-                I am Yannis.
-            </h2>
-
             <h3 className="heading  font-bold flex h-fit ">
-                I build stuff for the web.
+                I build stuff for<br/>the web.
             </h3>
             <p className="">
             I work as a Full Stack Developer, expanding the skills attained through rigorous training and hands-on experience as well as various courses. 
